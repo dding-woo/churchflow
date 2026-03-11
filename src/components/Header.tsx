1a9240704f7311cedffd3a13f2dfbdd12858c549
dding-woo <hsw7852@naver.com>
2026-03-11 17:26:27 +0900

    "add seo mange + index.html + robots.txt + sitemap.xml"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => (
     <h1 className="text-5xl font-extrabold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-primary-300 to-white">
       Lyrics to PPT
     </h1>
-    <p className="text-slate-400 text-lg">가사를 프레젠테이션으로 즉시 변환하세요.</p>
+    <p className="text-slate-400 text-lg">찬양 가사를 프레젠테이션으로 즉시 변환하세요.</p>
   </header>
 );
 
